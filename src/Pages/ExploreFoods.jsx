b9e0ba957773da0a5dce7723f745ec59da06f69e
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-06-30 00:05:55 -0300

    requisitos 70 a 73 completos

--- src/Pages/ExploreFoods.jsx
+++ src/Pages/ExploreFoods.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
 function ExploreFoods() {
+  const history = useHistory();
   return (
     <div>
       <Header />
+      <button
+        data-testid="explore-by-ingredient"
+        type="button"
+        onClick={ () => history.push('/explore/foods/ingredients') }
+      >
+        By Ingredient
+      </button>
+      <button
+        data-testid="explore-by-nationality"
+        type="button"
+        onClick={ () => history.push('/explore/foods/nationalities') }
+      >
+        By Nationality
+      </button>
+      <button data-testid="explore-surprise" type="button">
+        Surprise me!
+      </button>
+
       <Footer />
     </div>
   );

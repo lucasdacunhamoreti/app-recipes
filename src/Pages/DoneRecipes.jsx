1af788232e0f44dd1a9c79eca11068a7ef437c37
Andre <aa.andremacedo@gmail.com>
2022-06-29 21:14:55 -0300

    feat: requisito 54

--- src/Pages/DoneRecipes.jsx
+++ src/Pages/DoneRecipes.jsx
@@ -3,55 +3,82 @@ import React from 'react';
 import Header from '../Components/Header';
 // import { useParams } from 'react-router-dom';
 
-// function DoneRecipes({ recipe }) {
 function DoneRecipes() {
   // const [isFavorited, setIsFavorited] = useState(false);
   // const [alertCopyboard, setAlertCopyboard] = useState(false);
   // const { id } = useParams();
-  // function handleDone() {
-  //   const doneRecipes = {
-  //     id: '',
-  //     type: '',
-  //     nationality: '',
-  //     category: '',
-  //     alcoholicOrNot: '',
-  //     name: '',
-  //     image: '',
-  //     doneDate: '',
-  //     tags: '',
-  //   };
 
-  //   const doneRecipeString = JSON.stringify([doneRecipes]);
-  //   if (!localStorage.getItem('doneRecipes')) {
-  //     localStorage.setItem('doneRecipes', doneRecipeString);
-  //     // setIsFavorited(true);
-  //   } else {
-  //     const local = JSON.parse(localStorage.getItem('doneRecipes'));
-  //   }
-  // }
+  const localDone = JSON.parse(localStorage.getItem('doneRecipes'));
+  // const localDone = (localStorage.getItem('doneRecipes'));
+  localDone.forEach((local) => console.log('local', local));
 
   return (
     <div>
       <Header />
-      <div>
+      <section>
         <button data-testid="filter-by-all-btn" type="button">All</button>
         <button data-testid="filter-by-food-btn" type="button">Food</button>
         <button data-testid="filter-by-drink-btn" type="button">Drinks</button>
-      </div>
+      </section>
 
-      <div>
-        {/* <img data-testid={ `${index}-horizontal-image` } src={ img } alt="" />
-        <span data-testid={ `${index}-horizontal-top-text` }>Categoria</span>
-        <span data-testid={ `${index}-horizontal-name` }>Nome da Receita</span>
-        <span data-testid={ `${index}-horizontal-done-date` }>Data</span> */}
-        <button
-          // data-testid={ `${index}-horizontal-share-btn` }
-          type="button"
-        >
-          Compartilhar
-        </button>
-        {/* <span data-testid={ `${index}-${tagName}-horizontal-tag` }>tags</span> */}
-      </div>
+      <section>
+        {/* {localDone.map((recipe, index) => { */}
+        {localDone.map((recipe, index) => {
+          const { tags } = recipe;
+          const tagList = [];
+          if (typeof (tags) === 'string') {
+            tagList.push(tags?.split(','));
+          }
+          // console.log(tags);
+          console.log(tagList);
+          return (
+            <div key={ index }>
+              <div>
+                <img
+                  data-testid={ `${index}-horizontal-image` }
+                  src={ recipe.image }
+                  alt=""
+                />
+                <span data-testid={ `${index}-horizontal-name` }>{recipe.name}</span>
+                <span
+                  data-testid={ `${index}-horizontal-top-text` }
+                >
+                  {recipe.category}
+                </span>
+                <span
+                  data-testid={ `${index}-horizontal-done-date` }
+                >
+                  {recipe.doneData}
+                </span>
+                <button
+                  data-testid={ `${index}-horizontal-share-btn` }
+                  type="button"
+                >
+                  Compartilhar
+                </button>
+
+                { tagList
+                  ? tagList.map((tag) => {
+                    console.log(tag);
+                    return (
+                      <span
+                        key={ index + index }
+                        data-testid={ `${index}-${tag}-horizontal-tag` }
+                      >
+                        {tag}
+                      </span>
+                    );
+                  })
+                  : (
+                    <span
+                      key={ index + index }
+                      data-testid={ `${index}-${'Pasta'}-horizontal-tag` }
+                    />)}
+              </div>
+            </div>);
+        })}
+      </section>
+
     </div>
   );
 }
